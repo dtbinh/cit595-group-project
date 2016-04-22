7b5b176afd80a189f2c65b9b3b191727c77e774a
Kowasaki <weiyinko@gmail.com>
2016-04-22 12:26:40 -0400

    updated C

--- server.c
+++ server.c
@@ -1,233 +1,137 @@
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <errno.h>
-#include <string.h>
-#include <fcntl.h>
-#include <signal.h>
-#include <termios.h>
-#include <unistd.h>
-#include <pthread.h>
-#include "linkedlist.h"
+#include <pebble.h>
+static Window *window;
+static TextLayer *hello_layer;
+static char msg[100];
 
-
-char buffer[20];
-char big_buffer[10000];
-int fdusb;
-node* head;
-
-void clear_buffer() {
-    int i;
-    for (i = 0; i < 20; i++) {
-        buffer[i] = '\0';
-    }
+/* This is called when the down button is clicked */
+void down_click_handler(ClickRecognizerRef recognizer, void *context) {
+  text_layer_set_text(hello_layer, "Standby!"); 
+  DictionaryIterator *iter;
+ app_message_outbox_begin(&iter);
+ int key = 1;
+  
+ // send the message "standby" to the phone, using key #1
+ Tuplet value = TupletCString(key, "standby");
+ dict_write_tuplet(iter, &value);
+ app_message_outbox_send();
 }
 
-void clear_big_buffer() {
-    int i;
-    for (i = 0; i < 10000; i++) {
-        big_buffer[i] = '\0';
-    }
+
+void out_sent_handler(DictionaryIterator *sent, void *context) {
+ // outgoing message was delivered -- do nothing
 }
 
-void receive_data() {
-    fdusb = open("/dev/cu.usbmodem1421", O_RDWR);
-    int i;
-    struct termios options; // struct to hold options
-    tcgetattr(fdusb, &options);  // associate with this fd
-    cfsetispeed(&options, 9600); // set input baud rate
-    cfsetospeed(&options, 9600); // set output baud rate
-    tcsetattr(fdusb, TCSANOW, &options); // set options
-    int firstnewline;
-    int secondnewline;
-    char temperature[20];
-    while(1) {
-        //clear_buffer();
-        int bytes_read;
-        if (fdusb == -1) {
-            printf("We messed up\n");
-            return;
-        }
-        if((bytes_read = read(fdusb, buffer, 20)) > 0) {
-            printf("Buffer: %s\n", buffer);
-            buffer[bytes_read] = '\0';
-            strcat(big_buffer, buffer);
-            firstnewline = -1;
-            secondnewline = -1;
-            for (i = 0; i < strlen(big_buffer); i++) {
-                if (firstnewline == -1 && big_buffer[i] == 'T') {
-                    firstnewline = i;
-                    printf("Firstline: %d\n", firstnewline);
-                } else if (firstnewline >= 0 && big_buffer[i] == ';') {
-                    secondnewline = i;
-                    printf("Secondline: %d\n", secondnewline);
-                    break;
-                }
-            }
-        }
-        if (secondnewline > 0) {
-            big_buffer[secondnewline] = '\0';
-            firstnewline = firstnewline + 2;
-            strcpy(temperature, &big_buffer[firstnewline]);
-            head = add_to_list(head, atof(temperature));
-        }
-        clear_buffer();
-        printf("Buffer cleared!\n");
-        head = trim_list(head);
-    }
-    
+void out_failed_handler(DictionaryIterator *failed,
+ AppMessageResult reason, void *context) {
+ // outgoing message failed
+   text_layer_set_text(hello_layer, "Error out!");
 }
 
-int start_server(int PORT_NUMBER)
+void in_received_handler(DictionaryIterator *received, void *context) {
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "MESSAGE RECEIVED");
+  // incoming message received
+  // looks for key #0 in the incoming message
+  int key = 0;
+  Tuple *text_tuple = dict_find(received, key);
+  if (text_tuple) {
+    if (text_tuple->value) {
+      // put it in this global variable
+      strcpy(msg, text_tuple->value->cstring);
+    } else strcpy(msg, "no value!");
+
+    text_layer_set_overflow_mode(hello_layer, GTextOverflowModeWordWrap);
+    text_layer_set_text(hello_layer, msg);   
+
+
+  } else {
+    text_layer_set_text(hello_layer, "no message!");
+  }
+}
+
+void in_dropped_handler(AppMessageResult reason, void *context) {
+ // incoming message dropped
+ text_layer_set_text(hello_layer, "Error in!");
+}
+
+
+/* This is called when the select button is clicked */
+void select_click_handler(ClickRecognizerRef recognizer, void *context)
 {
+
+ DictionaryIterator *iter;
+ app_message_outbox_begin(&iter);
+ int key = 0;
+  
+ // send the message "hello?" to the phone, using key #0
+ Tuplet value = TupletCString(key, "hello?");
+ dict_write_tuplet(iter, &value);
+ app_message_outbox_send();
     
-    // structs to represent the server and client
-    struct sockaddr_in server_addr,client_addr;
-    
-    int sock, error, temp, i; // socket descriptor
-    
-    // 1. socket: creates a socket descriptor that you later use to make other system calls
-    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
-        perror("Socket");
-        exit(1);
-    }
-    if (setsockopt(sock,SOL_SOCKET,SO_REUSEADDR,&temp,sizeof(int)) == -1) {
-        perror("Setsockopt");
-        exit(1);
-    }
-    
-    // configure the server
-    server_addr.sin_port = htons(PORT_NUMBER); // specify port number
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.s_addr = INADDR_ANY;
-    bzero(&(server_addr.sin_zero),8);
-    
-    // 2. bind: use the socket and associate it with the port number
-    if (bind(sock, (struct sockaddr *)&server_addr, sizeof(struct sockaddr)) == -1) {
-        perror("Unable to bind");
-        exit(1);
-    }
-    
-    // 3. listen: indicates that we want to listn to the port to which we bound; second arg is number of allowed connections
-    if (listen(sock, 5) == -1) {
-        perror("Listen");
-        exit(1);
-    }
-    
-    // once you get here, the server is set up and about to start listening
-    printf("\nServer configured to listen on port %d\n", PORT_NUMBER);
-    fflush(stdout);
-    
-    
-    // 4. accept: wait here until we get a connection on that port
-    int sin_size = sizeof(struct sockaddr_in);
-    int fd;
-    big_buffer[0] = '\0';
-    pthread_t usbreader;
-    error = pthread_create(&usbreader, NULL, receive_data, NULL);
-    if (error != 0) {
-        printf("Error in thread creation\n");
-        return -1;
-    }
-    
-    while(1) {
-        printf("Waiting...\n");
-        fd = accept(sock, (struct sockaddr *)&client_addr,(socklen_t *)&sin_size);
-        printf("Accepted...\n");
-        printf("Server got a connection from (%s, %d)\n", inet_ntoa(client_addr.sin_addr),ntohs(client_addr.sin_port));
-        
-        // buffer to read data into
-        char request[1024];
-        
-        // 5. recv: read incoming message into buffer
-        int bytes_received = recv(fd,request,1024,0);
-        // null-terminate the string
-        request[bytes_received] = '\0';
-        printf("Here comes the message:\n");
-        printf("%s\n", request);
-        
-        /* Assumes a GET request. We need an if/else to differentiate between GET/POST.*/
-        
-        /*
-         If GET
-         ... same as existing code below
-         Else if POST
-         Parse string for API temperature
-         Store API temperature variable
-         Compare API temperature variable with test variable (e.g. 70 degrees F)
-         Set color variable to send to Arduino:
-         If API > Arduino: red
-         Else if API < Arduino: blue
-         Else: green
-         Send to Arduino
-         
-         */
-        
-        
-        if (request[0] == 'G') {
-            clear_big_buffer();
-            char reply[200];
-            sprintf(reply, "{\n\"name\": \"%f\"\n}\n", get_latest(head));
-            
-            // 6. send: send the message over the socket
-            // note that the second argument is a char*, and the third is the number of chars
-            bytes_received =send(fd, reply, strlen(reply), 0);
-            printf("%d\n", bytes_received);
-            printf("Server sent message: %s\n", reply);
-            
-            // 7. close: close the socket connection
-            close(fd);
-            
-        } else if (request[0] == 'P') {
-            // 6. send: send the message over the socket
-            // note that the second argument is a char*, and the third is the number of chars
-            
-            
-            int bytes_read;
-            int bytes_wrote;
-            
-            bytes_wrote = write(fdusb, "Hello!", strlen("Hello!"));
-            printf("%d\n", bytes_wrote);
-            // 7. close: close the socket connection
-            bytes_received =send(fd, request, strlen(request), 0);
-            close(fd);
-        }
-    }
-    // this is the message that we'll send back
-    /* it actually looks like this:
-     {
-     "name": "cit595"
-     }
-     */
-    error = pthread_join(usbreader, NULL);
-    if (error != 0) {
-        printf("Error in thread join\n");
-        return -1;
-    }
-    
-    close(sock);
-    printf("Server closed connection\n");
-    
-    return 0;
+ //text_layer_set_text(hello_layer, "Selected!");
+
 }
 
 
+/* this registers the appropriate function to the appropriate button */
+void config_provider(void *context) {
+  uint8_t min_clicks = 2;          // Fire after at least one clicks
+  uint8_t max_clicks = 2;          // Don't fire after two clicks
+  uint16_t timeout = 0;          // Wait 300ms before firing
+  bool last_click_only = true;     // Fire only after the last click
+  
+  window_single_click_subscribe(BUTTON_ID_DOWN, down_click_handler);
 
-int main(int argc, char *argv[])
-{
-    // check the number of arguments
-    if (argc != 2)
-    {
-        printf("\nUsage: server [port_number]\n");
-        exit(0);
-    }
-    
-    int PORT_NUMBER = atoi(argv[1]);
-    head = NULL;
-    start_server(PORT_NUMBER);
+  ButtonId id = BUTTON_ID_DOWN;  // The Down button
+  window_multi_click_subscribe(id, min_clicks, max_clicks, timeout,
+                               last_click_only, select_click_handler);
+  
+  window_single_click_subscribe(BUTTON_ID_SELECT, select_click_handler);
 }
 
+
+static void window_load(Window *window) {
+ Layer *window_layer = window_get_root_layer(window);
+ GRect bounds = layer_get_bounds(window_layer);
+ hello_layer = text_layer_create((GRect)
+ { .origin = { 0, 72 },
+ .size = { bounds.size.w, 50 } });
+ text_layer_set_text(hello_layer, "Use OpenWeather!");
+ text_layer_set_text_alignment(hello_layer, GTextAlignmentCenter);
+ layer_add_child(window_layer, text_layer_get_layer(hello_layer));
+}
+
+static void window_unload(Window *window) {
+ text_layer_destroy(hello_layer);
+}
+
+static void init(void) {
+ window = window_create();
+ window_set_window_handlers(window, (WindowHandlers) {
+ .load = window_load,
+ .unload = window_unload,
+ });
+// need this for adding the listener
+window_set_click_config_provider(window, config_provider);
+
+ // for registering AppMessage handlers
+ app_message_register_inbox_received(in_received_handler);
+ app_message_register_inbox_dropped(in_dropped_handler);
+ app_message_register_outbox_sent(out_sent_handler);
+ app_message_register_outbox_failed(out_failed_handler);
+ const uint32_t inbound_size = 64;
+ const uint32_t outbound_size = 64;
+ app_message_open(inbound_size, outbound_size);
+
+ const bool animated = true;
+ window_stack_push(window, animated);
+}
+
+static void deinit(void) {
+ window_destroy(window);
+}
+
+int main(void) {
+ init();
+ app_event_loop();
+ deinit();
+}
