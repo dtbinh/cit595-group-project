2fb1ef1e8de44f6218aae80d1bd8c959b88efdf4
Kowasaki <weiyinko@gmail.com>
2016-04-22 12:03:07 -0400

    oops trying again

--- app.js
+++ app.js
@@ -4,16 +4,72 @@ var sensorWork = true;
 
 Pebble.addEventListener("appmessage",
   function(e) {
-    getCity();
+//     console.log(e.payload);
+    console.log(JSON.stringify(e));
+//     test();
+//     getCity();
 //     getTemp();
-    setTimeout(checkConnect, 2000);
-    setTimeout(sendToServer, 3000);
+//     setTimeout(checkConnect, 2000);
+//     setTimeout(sendToServer, 3000);
+    checkPayload(e);
     
  }
 );
 
-function test(){
-   Pebble.sendAppMessage({ "1": 'down button message!' });
+function checkPayload(e){
+    for (var key in e.payload) break;
+    if(key === '0'){
+       getCity();
+       setTimeout(checkConnect, 2000);
+       setTimeout(sendToServer, 3000);
+    } else if(key === '1'){
+       sendStandBy();
+    }
+}
+
+function sendStandBy(){
+    Pebble.sendAppMessage({ "0": 'sending standby!' });
+    var req = new XMLHttpRequest();
+    var ipAddress = "158.130.110.173"; // Hard coded IP address
+    var port = "3001"; // Same port specified as argument to server
+    var url = "http://" + ipAddress + ":" + port + "/";
+    var method = "GET";
+    var sendStuff = "POST";
+    var async = true;
+    var standby = 'STANDBY';
+
+    req.open(sendStuff, url, async);
+    
+    console.log(req.status);
+    console.log(standby);
+    req.send(standby);
+//     console.log('current temp is ' + temp);
+    req.onload = function(e) {
+      // see what came back
+      var msg = "no response";
+  
+      var response = JSON.parse(req.responseText);
+      console.log("GOT SOMETHING");
+      console.log(JSON.stringify(response));
+      if (response) {
+        if (response.name) {
+          msg = response.name;
+          lastAttempt = true;
+          if(msg === 'no sensor'){
+            sensorWork = false;
+          }
+        } 
+        // sends message back to pebble
+        Pebble.sendAppMessage({ "0": msg });
+      } else{
+          Pebble.sendAppMessage({ "0": 'uh-oh...' });
+          lastAttempt = false;       
+          Pebble.sendAppMessage({ "0": 'phone connection lost!' });
+        }
+      req.open(method, url, async);
+      req.send(null);
+    };
+
 }
 
 function checkConnect(){
@@ -31,16 +87,14 @@ function checkConnect(){
 
 }
 
-function getCity(){
+function getCity(e){
   var key = '863fad9850866cd53fbf3c264f6d4631';
   var req = new XMLHttpRequest();
   var msg = 'Empty Msg!';
-//   var temp;
-  
+//   for (var Pkey in e.payload) break;
+//   if(Pkey === '0'){
   req.open('GET', 'http://api.openweathermap.org/data/2.5/weather?' +
   'lat=' + 39.98 + '&lon=' + -75.19  + '&cnt=1&appid=' + key, true);
-//   req.open('GET', 'http://api.openweathermap.org/data/2.5/forecast/city?id=524901&appid=863fad9850866cd53fbf3c264f6d4631', true);
-//   var source = 'http://api.openweathermap.org/data/2.5/forecast/city?id=524901&appid=863fad9850866cd53fbf3c264f6d4631';
 
   req.onload = function(e) {
 //      console.log("in onload");
@@ -61,6 +115,7 @@ function getCity(){
   };
   req.send(null);
   return msg;
+//   }
 }
 
 function getTemp(){
@@ -68,6 +123,8 @@ function getTemp(){
   var req = new XMLHttpRequest();
   var msg = 'No Temp!';
 //   var celsius;  
+//   for (var Pkey in e.payload) break;
+//   if(Pkey === '0'){
   req.open('GET', 'http://api.openweathermap.org/data/2.5/weather?' +
   'lat=' + 39.98 + '&lon=' + -75.19  + '&cnt=1&appid=' + key, true);
 
@@ -75,7 +132,7 @@ function getTemp(){
     msg = "failed";
     var response = JSON.parse(req.responseText);
     console.log("returned something!");
-    console.log(JSON.stringify(response));
+//     console.log(JSON.stringify(response));
     if (response) {
       if (response.name) {
         msg = response.main.temp;
@@ -90,54 +147,58 @@ function getTemp(){
   req.send(null);
   
   return celsius;
+//   }
 }
 
 function sendToServer() {
-  var req = new XMLHttpRequest();
-  var ipAddress = "158.130.110.173"; // Hard coded IP address
-  var port = "3001"; // Same port specified as argument to server
-  var url = "http://" + ipAddress + ":" + port + "/";
-  var method = "GET";
-  var sendStuff = "POST";
-  var async = true;
-//   var city = getCity();
-  var temp = getTemp();
-  temp = Math.round(temp * 100) / 100;
-  console.log('temperature is ' + temp);
-  var strTemp = temp.toString();
-  Pebble.sendAppMessage({ "0": 'Now comaparing temperature...' });
-//   console.log(strTemp);
-  req.open(sendStuff, url, async);
-  console.log(req.status);
-  req.send(strTemp);
-  console.log('current temp is ' + temp);
-  req.onload = function(e) {
-    // see what came back
-    var msg = "no response";
-
-    var response = JSON.parse(req.responseText);
-    console.log("GOT SOMETHING");
-    console.log(JSON.stringify(response));
-//     if(JSON.stringify(response) === 'none'){
-//        Pebble.sendAppMessage({ "0": 'no response from server!' });
-//     }
-    if (response) {
-      if (response.name) {
-        msg = response.name;
-        lastAttempt = true;
-        if(msg === 'no sensor'){
-          sensorWork = false;
+//   for (var Pkey in e.payload) break;
+//   if(Pkey === '0'){
+    var req = new XMLHttpRequest();
+    var ipAddress = "158.130.110.173"; // Hard coded IP address
+    var port = "3001"; // Same port specified as argument to server
+    var url = "http://" + ipAddress + ":" + port + "/";
+    var method = "GET";
+    var sendStuff = "POST";
+    var async = true;
+  //   var city = getCity();
+    var temp = getTemp();
+    temp = Math.round(temp * 100) / 100;
+    console.log('temperature is ' + temp);
+    var strTemp = temp.toString();
+    Pebble.sendAppMessage({ "0": 'Now comaparing temperature...' });
+  //   console.log(strTemp);
+    req.open(sendStuff, url, async);
+    console.log(req.status);
+    req.send(strTemp);
+    console.log('current temp is ' + temp);
+    req.onload = function(e) {
+      // see what came back
+      var msg = "no response";
+  
+      var response = JSON.parse(req.responseText);
+      console.log("GOT SOMETHING");
+      console.log(JSON.stringify(response));
+  //     if(JSON.stringify(response) === 'none'){
+  //        Pebble.sendAppMessage({ "0": 'no response from server!' });
+  //     }
+      if (response) {
+        if (response.name) {
+          msg = response.name;
+          lastAttempt = true;
+          if(msg === 'no sensor'){
+            sensorWork = false;
+          }
+        } 
+        // sends message back to pebble
+        Pebble.sendAppMessage({ "0": msg });
+      } else{
+          Pebble.sendAppMessage({ "0": 'uh-oh...' });
+          lastAttempt = false;
+        
+          Pebble.sendAppMessage({ "0": 'phone connection lost!' });
         }
-      } 
-      // sends message back to pebble
-      Pebble.sendAppMessage({ "0": msg });
-    } else{
-        Pebble.sendAppMessage({ "0": 'uh-oh...' });
-        lastAttempt = false;
-      
-        Pebble.sendAppMessage({ "0": 'phone connection lost!' });
-      }
-    req.open(method, url, async);
-    req.send(null);
-  };
+      req.open(method, url, async);
+      req.send(null);
+    };
+//   }
 }
